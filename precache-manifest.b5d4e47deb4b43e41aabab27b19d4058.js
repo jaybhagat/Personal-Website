d780cc709e76369a0aa6f67fbc2b87d341d8918c
Jay Bhagat <jay@Jays-MacBook-Pro.local>
2020-01-18 20:04:53 -0500

    Updates

---
+++ precache-manifest.b5d4e47deb4b43e41aabab27b19d4058.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01214e50bdf37eb60d1c05f6a04c671",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8ff18f1322004c9e0d6",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "3afee6614b9de6f36db9",
+    "url": "/static/css/main.4bf3d2b5.chunk.css"
+  },
+  {
+    "revision": "d8ff18f1322004c9e0d6",
+    "url": "/static/js/2.d5f0d328.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.d5f0d328.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3afee6614b9de6f36db9",
+    "url": "/static/js/main.bd94e279.chunk.js"
+  },
+  {
+    "revision": "5db6dfe7a8e2b4135dd2",
+    "url": "/static/js/runtime-main.aa739829.js"
+  }
+]);
